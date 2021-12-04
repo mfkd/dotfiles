136dc98d490e0107253e1532befbb2d152e10e56
mfkd <mattkfd@gmail.com>
2021-12-04 05:40:35 +0100

    nvim: remove deprecated indent-blankline option

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -107,7 +107,6 @@ vim.api.nvim_exec(
 vim.g.indent_blankline_char = '┊'
 vim.g.indent_blankline_filetype_exclude = { 'help', 'packer' }
 vim.g.indent_blankline_buftype_exclude = { 'terminal', 'nofile' }
-vim.g.indent_blankline_char_highlight = 'LineNr'
 vim.g.indent_blankline_show_trailing_blankline_indent = false
 
 -- Telescope
