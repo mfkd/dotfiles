ef84cdd864bc17095b7e34451ec77ec7512c8bca
mfkd <mattkfd@gmail.com>
2024-12-27 19:39:27 +0100

    fish: Add aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,24 @@
 set -U fish_greeting
 
+# Aliases
+alias d="docker"
+alias k="kubectl"
+alias g="git"
+alias by="bat -l yaml"
+
+alias ls="eza --icons --group-directories-first"
+alias l="eza --icons -l --all --group-directories-first"
+alias ll="eza --icons -l --all --all --group-directories-first --git"
+alias lt="eza --icons -T --git-ignore --level=2 --group-directories-first"
+alias llt="eza --icons -lT --git-ignore --level=2 --group-directories-first"
+alias lT="eza --icons -T --git-ignore --level=4 --group-directories-first"
+alias tree="eza --icons -T"
+
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+
 if status is-interactive
     zoxide init fish | source
 end
