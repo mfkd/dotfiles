3eecb44c2cf8477873242a5a726793950cb19436
mfkd <mattkfd@gmail.com>
2025-01-03 12:13:05 +0100

    fish: add bat as man pager

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,6 +4,7 @@ set -U fish_greeting
 # Environment variables
 set -gx VISUAL "nvim"
 set -gx EDITOR "nvim"
+set -gx MANPAGER "sh -c 'col -bx | bat -l man --paging=always'"
 
 # Add directories to PATH only if they exist
 set -l paths_to_add \
