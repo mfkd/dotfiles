c77c6ed366c06fd8b3f970304e9c34c5b829aa01
mfkd <mattkfd@gmail.com>
2025-01-26 07:28:34 +0100

    fish: remove FZF_DEFAULT_OPTS coloring

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -47,22 +47,10 @@ alias vim="nvim"
 
 function dark
     echo "y" | fish_config theme save "Catppuccin Mocha"
-    set -Ux FZF_DEFAULT_OPTS "\
-    --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
-    --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
-    --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
-    --color=selected-bg:#45475a \
-    --multi"
 end
 
 function light
     echo "y" | fish_config theme save "Catppuccin Latte"
-    set -Ux FZF_DEFAULT_OPTS "\
-    --color=bg+:#ccd0da,bg:#eff1f5,spinner:#dc8a78,hl:#d20f39 \
-    --color=fg:#4c4f69,header:#d20f39,info:#8839ef,pointer:#dc8a78 \
-    --color=marker:#7287fd,fg+:#4c4f69,prompt:#8839ef,hl+:#d20f39 \
-    --color=selected-bg:#bcc0cc \
-    --multi"
 end
 
 function set_theme
