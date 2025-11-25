b72ce5449acf13ca61c1e941cccfc02e688fb3cc
mfkd <mattkfd@gmail.com>
2025-11-25 12:27:58 +0100

    fish: simply help function and remove abbr.fish guard

--- .config/fish/conf.d/40-abbr.fish
+++ .config/fish/conf.d/40-abbr.fish
@@ -1,10 +1,6 @@
-if not set -q MY_ABBR_SET
-    set -U MY_ABBR_SET true
-
-    abbr -a c  codex
-    abbr -a g  git
-    abbr -a k  kubectl
-    abbr -a d  docker
-    abbr -a vi nvim
-    abbr -a vim nvim
-end
+abbr -a c  codex
+abbr -a g  git
+abbr -a k  kubectl
+abbr -a d  docker
+abbr -a vi nvim
+abbr -a vim nvim
--- .config/fish/functions/help.fish
+++ .config/fish/functions/help.fish
@@ -1,13 +1,3 @@
 function help
-    if test (count $argv) -eq 0
-        command help
-        return
-    end
-
-    if type -q $argv[1]
-        command $argv --help 2>&1 | bathelp
-    else
-        echo "Unknown command: $argv[1]" >&2
-        return 1
-    end
+    $argv --help 2>&1 | bathelp
 end
