6e4f8d8cc016b6d97039c2d5bc35cabf2f4e7898
mfkd <mattkfd@gmail.com>
2025-11-25 12:58:00 +0100

    fish: remove guard for zoxide and starship

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,8 @@
 if status is-interactive
-    # Optional tools, guarded so they don't error if missing
-    type -q zoxide;   and zoxide init --cmd cd fish | source
-    type -q starship; and starship init fish | source
+    zoxide init --cmd cd fish | source
+    starship init fish | source
 
-    functions -q enable_transience; and enable_transience
+    enable_transience
 
     # Auto-switch theme based on macOS appearance
     set_theme
