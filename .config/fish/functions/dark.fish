c55993684794dd38f8c991920da745bb36ea7b8b
mfkd <mattkfd@gmail.com>
2025-12-19 11:58:08 +0200

    fish: Use starship catppuccin dynamically

--- .config/fish/functions/dark.fish
+++ .config/fish/functions/dark.fish
@@ -2,5 +2,6 @@ function dark
     if test "$fish_theme" != "Catppuccin Mocha"
         echo y | fish_config theme save "Catppuccin Mocha"
         set -Ux fish_theme "Catppuccin Mocha"
+        starship config palette catppuccin_mocha
     end
 end
--- .config/fish/functions/light.fish
+++ .config/fish/functions/light.fish
@@ -2,5 +2,6 @@ function light
     if test "$fish_theme" != "Catppuccin Latte"
         echo y | fish_config theme save "Catppuccin Latte"
         set -Ux fish_theme "Catppuccin Latte"
+        starship config palette catppuccin_latte
     end
 end
