8a8bf44d30ec05606d4c9834c017413aac178ec8
mfkd <mattkfd@gmail.com>
2025-12-24 09:04:04 +0200

    Fish: Add eza catppuccin theme and set dynamically in fish

--- .config/fish/functions/dark.fish
+++ .config/fish/functions/dark.fish
@@ -3,5 +3,6 @@ function dark
         echo y | fish_config theme save "Catppuccin Mocha"
         set -Ux fish_theme "Catppuccin Mocha"
         starship config palette catppuccin_mocha
+        set -Ux EZA_CONFIG_DIR ~/.config/eza/dark
     end
 end
--- .config/fish/functions/light.fish
+++ .config/fish/functions/light.fish
@@ -3,5 +3,6 @@ function light
         echo y | fish_config theme save "Catppuccin Latte"
         set -Ux fish_theme "Catppuccin Latte"
         starship config palette catppuccin_latte
+        set -Ux EZA_CONFIG_DIR ~/.config/eza/light
     end
 end
