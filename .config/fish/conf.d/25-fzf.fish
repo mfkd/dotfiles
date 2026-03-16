fe6b10a17ae4939db7b04add3fa8c2c6f04c6210
mfkd <mattkfd@gmail.com>
2026-03-16 13:56:46 +0000

    Migrate fish to official fzf integration

---
+++ .config/fish/conf.d/25-fzf.fish
@@ -0,0 +1,33 @@
+if not status is-interactive
+    return
+end
+
+if not command -sq fzf
+    return
+end
+
+set -gx FZF_DEFAULT_OPTS "\
+--height=40% \
+--layout=reverse \
+--border"
+
+set -gx FZF_CTRL_T_COMMAND "command fd --hidden --follow --exclude .git --exclude node_modules --exclude target . \$dir"
+set -gx FZF_CTRL_T_OPTS "\
+--preview 'if [ -d {} ]; then eza --tree --all --level=2 --color=always {}; else bat -n --color=always --line-range :500 {}; fi' \
+--bind 'ctrl-/:change-preview-window(down|hidden|)'"
+
+set -gx FZF_CTRL_R_OPTS "\
+--bind 'ctrl-y:execute-silent(echo -n {2..} | pbcopy)+abort' \
+--color header:italic \
+--header 'Press CTRL-Y to copy command'"
+
+set -gx FZF_ALT_C_COMMAND "command fd --type d --hidden --follow --exclude .git --exclude node_modules --exclude target . \$dir"
+set -gx FZF_ALT_C_OPTS "\
+--preview 'eza --tree --all --level=2 --color=always {}' \
+--bind 'ctrl-/:change-preview-window(down|hidden|)'"
+
+set -gx FZF_COMPLETION_OPTS "\
+--border \
+--info=inline"
+
+fzf --fish | source
--- .config/fish/functions/__apply_theme.fish
+++ .config/fish/functions/__apply_theme.fish
@@ -1,14 +1,17 @@
 function __apply_theme --argument mode
     set -l eza_config_dir
     set -l starship_config
+    set -l bat_theme_mode
 
     switch "$mode"
         case dark
             set eza_config_dir "$HOME/.config/eza/dark"
             set starship_config "$HOME/.config/starship-dark.toml"
+            set bat_theme_mode dark
         case light
             set eza_config_dir "$HOME/.config/eza/light"
             set starship_config "$HOME/.config/starship-light.toml"
+            set bat_theme_mode light
         case "*"
             echo "__apply_theme: unsupported mode '$mode'" >&2
             return 1
@@ -16,4 +19,5 @@ function __apply_theme --argument mode
 
     set -gx EZA_CONFIG_DIR "$eza_config_dir"
     set -gx STARSHIP_CONFIG "$starship_config"
+    set -gx BAT_THEME "$bat_theme_mode"
 end
