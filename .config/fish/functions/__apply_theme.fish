7362f66ac379a788eed53d9708d6379e05d33cd3
mfkd <mattkfd@gmail.com>
2026-03-17 20:22:34 +0000

    fish: stop exporting BAT_THEME from theme switching

--- .config/fish/functions/__apply_theme.fish
+++ .config/fish/functions/__apply_theme.fish
@@ -1,17 +1,14 @@
 function __apply_theme --argument mode
     set -l eza_config_dir
     set -l starship_config
-    set -l bat_theme_mode
 
     switch "$mode"
         case dark
             set eza_config_dir "$HOME/.config/eza/dark"
             set starship_config "$HOME/.config/starship-dark.toml"
-            set bat_theme_mode dark
         case light
             set eza_config_dir "$HOME/.config/eza/light"
             set starship_config "$HOME/.config/starship-light.toml"
-            set bat_theme_mode light
         case "*"
             echo "__apply_theme: unsupported mode '$mode'" >&2
             return 1
@@ -19,7 +16,7 @@ function __apply_theme --argument mode
 
     set -gx EZA_CONFIG_DIR "$eza_config_dir"
     set -gx STARSHIP_CONFIG "$starship_config"
-    set -gx BAT_THEME "$bat_theme_mode"
+    set -e BAT_THEME
 
     if set -q FZF_DEFAULT_OPTS_BASE[1]
         __set_fzf_theme "$mode"
