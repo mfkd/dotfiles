4b8521ed72f2d168bc769a21669b674ae61764bd
mfkd <mattkfd@gmail.com>
2026-03-18 20:05:27 +0000

    Add eza icons to fzf previews

--- .config/fish/conf.d/25-fzf.fish
+++ .config/fish/conf.d/25-fzf.fish
@@ -15,7 +15,7 @@ set -gx FZF_DEFAULT_OPTS "$FZF_DEFAULT_OPTS_BASE"
 
 set -gx FZF_CTRL_T_COMMAND "command fd --hidden --follow --exclude .git --exclude node_modules --exclude target . \$dir"
 set -gx FZF_CTRL_T_OPTS "\
---preview 'if [ -d {} ]; then eza --tree --all --level=2 --color=always {}; else bat -n --color=always --line-range :500 {}; fi' \
+--preview 'if [ -d {} ]; then eza --icons --tree --all --level=2 --color=always {}; else bat -n --color=always --line-range :500 {}; fi' \
 --bind 'ctrl-/:change-preview-window(down|hidden|)'"
 
 set -gx FZF_CTRL_R_OPTS "\
@@ -25,7 +25,7 @@ set -gx FZF_CTRL_R_OPTS "\
 
 set -gx FZF_ALT_C_COMMAND "command fd --type d --hidden --follow --exclude .git --exclude node_modules --exclude target . \$dir"
 set -gx FZF_ALT_C_OPTS "\
---preview 'eza --tree --all --level=2 --color=always {}' \
+--preview 'eza --icons --tree --all --level=2 --color=always {}' \
 --bind 'ctrl-/:change-preview-window(down|hidden|)'"
 
 set -gx FZF_COMPLETION_OPTS "\
