80ccfb451515248a58284d9d99a2ce584d5dcebf
mfkd <mattkfd@gmail.com>
2026-03-18 20:07:11 +0000

    Add lazygit fish abbreviation

--- .config/fish/conf.d/40-abbr.fish
+++ .config/fish/conf.d/40-abbr.fish
@@ -2,9 +2,9 @@ abbr -a cx  codex
 abbr -a c claude
 abbr -a o opencode
 abbr -a g  git
+abbr -a lg lazygit
 abbr -a k  kubectl
 abbr -a d  docker
 abbr -a v nvim
 abbr -a vi nvim
 abbr -a vim nvim
-
