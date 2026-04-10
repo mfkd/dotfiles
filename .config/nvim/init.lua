cce9fa3ca852f44b2f72839a54f9ed14dae5c1d7
mfkd <mattkfd@gmail.com>
2026-04-10 14:44:21 +0100

    nvim: move core search mappings from Telescope to fff.nvim
    Add dmtrKovalenko/fff.nvim and remap the primary project search keys to it:
    - <leader>sf for file search
    - <leader>sg for live grep
    - <leader>sw for current word search
    - <leader>sz for FFF fuzzy/plain grep
    
    Keep Telescope installed for the broader picker set it still owns in this config,
    including help, keymaps, commands, diagnostics, buffers, resume, current-buffer
    search, open-file grep, and LSP pickers.
    
    Also add inline comments near the affected mappings so the reason for the split
    between FFF and Telescope is obvious when revisiting the config.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -227,6 +227,52 @@ require('lazy').setup({
     },
   },
 
+  { -- Fast file search and grep with frecency/history ranking
+    'dmtrKovalenko/fff.nvim',
+    build = function()
+      require('fff.download').download_or_build_binary()
+    end,
+    opts = {},
+    keys = {
+      {
+        -- Replaces Telescope's project file picker on <leader>sf.
+        '<leader>sf',
+        function()
+          require('fff').find_files()
+        end,
+        desc = '[S]earch [F]iles',
+      },
+      {
+        -- Replaces Telescope's project live grep on <leader>sg.
+        '<leader>sg',
+        function()
+          require('fff').live_grep()
+        end,
+        desc = '[S]earch by [G]rep',
+      },
+      {
+        -- Replaces Telescope's current-word search on <leader>sw.
+        '<leader>sw',
+        function()
+          require('fff').live_grep { query = vim.fn.expand '<cword>' }
+        end,
+        desc = '[S]earch current [W]ord',
+      },
+      {
+        -- Adds an FFF-only fuzzy grep entry point alongside the plain grep mapping.
+        '<leader>sz',
+        function()
+          require('fff').live_grep {
+            grep = {
+              modes = { 'fuzzy', 'plain' },
+            },
+          }
+        end,
+        desc = '[S]earch Fu[z]zy Grep',
+      },
+    },
+  },
+
   { -- Fuzzy Finder (files, lsp, etc)
     'nvim-telescope/telescope.nvim',
     event = 'VimEnter',
@@ -264,12 +310,10 @@ require('lazy').setup({
       pcall(require('telescope').load_extension, 'ui-select')
 
       local builtin = require 'telescope.builtin'
+      -- Keep Telescope for the broader picker set that FFF does not replace in this config.
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-      vim.keymap.set({ 'n', 'v' }, '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
